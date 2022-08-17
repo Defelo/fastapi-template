d24bcddbdcab512ed5729da3ed546e867a029be2
Defelo <elodef42@gmail.com>
2022-08-17 18:23:25 +0200

    Add documentation for test endpoints

--- api/endpoints/test.py
+++ api/endpoints/test.py
@@ -1,3 +1,5 @@
+"""Test endpoints (to be removed later)"""
+
 from typing import Any
 
 from fastapi import APIRouter
@@ -13,9 +15,17 @@ router = APIRouter(tags=["test"])
 
 @router.get("/test", responses=responses(TestResponse))
 async def test() -> Any:
+    """Test endpoint."""
+
     return {"result": "hello world"}
 
 
 @router.get("/auth", dependencies=[user_auth], responses=user_responses(list[int]))
 async def test_auth() -> Any:
+    """
+    Test endpoint with authentication.
+
+    *Requirements:* **USER**
+    """
+
     return [1, 2, 3]
--- api/schemas/test.py
+++ api/schemas/test.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from ..utils import example
 
 
 class TestResponse(BaseModel):
-    result: str
+    result: str = Field(description="Test result")
 
     Config = example(result="hello world")
