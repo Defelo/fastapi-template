cd9ce45427e2ff45e9ad4d59a62a6f8efc40dc49
Defelo <elodef42@gmail.com>
2022-08-22 15:17:31 +0200

    Improve oauth_user_connection model unit tests

--- tests/models/test_oauth_user_connection.py
+++ tests/models/test_oauth_user_connection.py
@@ -1,7 +1,4 @@
-from unittest.mock import AsyncMock
-
-from pytest_mock import MockerFixture
-
+from api.database import db, db_wrapper, select
 from api.models import OAuthUserConnection
 
 
@@ -10,14 +7,13 @@ async def test__serialize() -> None:
     assert obj.serialize == {"id": "connection_id_123", "provider_id": "my_oauth_provider", "display_name": "Foo Bar"}
 
 
-async def test__create(mocker: MockerFixture) -> None:
-    db = mocker.patch("api.models.oauth_user_connection.db", new_callable=AsyncMock)
-
+@db_wrapper
+async def test__create() -> None:
     obj = await OAuthUserConnection.create("user_id_123", "my_oauth_provider", "remote_user_id_123", "Foo Bar")
+    connections = await db.all(select(OAuthUserConnection))
+    assert connections == [obj]
 
     assert obj.user_id == "user_id_123"
     assert obj.provider_id == "my_oauth_provider"
     assert obj.remote_user_id == "remote_user_id_123"
     assert obj.display_name == "Foo Bar"
-
-    db.add.assert_called_once_with(obj)
