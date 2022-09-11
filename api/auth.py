09aa7d1d8a3890604af2016ffcd92bef192c171c
Defelo <elodef42@gmail.com>
2022-09-11 16:17:13 +0200

    Add force_valid parameter to jwt auth

--- api/auth.py
+++ api/auth.py
@@ -38,16 +38,22 @@ class StaticTokenAuth(HTTPAuth):
 
 
 class JWTAuth(HTTPAuth):
-    def __init__(self, audience: list[str] | None = None):
+    def __init__(self, *, audience: list[str] | None = None, force_valid: bool = True):
         super().__init__()
         self.audience: list[str] | None = audience
+        self.force_valid: bool = force_valid
 
-    async def __call__(self, request: Request) -> dict[Any, Any]:
-        if (data := decode_jwt(get_token(request), audience=self.audience)) is None:
+    async def __call__(self, request: Request) -> dict[Any, Any] | None:
+        if (data := decode_jwt(get_token(request), audience=self.audience)) is None and self.force_valid:
             raise InvalidTokenError
         return data
 
 
+class InternalAuth(JWTAuth):
+    def __init__(self, audience: list[str] | None = None):
+        super().__init__(audience=audience, force_valid=True)
+
+
 static_token_auth = Depends(StaticTokenAuth("secret token"))
 jwt_auth = Depends(JWTAuth())
-internal_auth = Depends(JWTAuth(audience=["service_xyz"]))
+internal_auth = Depends(InternalAuth(audience=["service_xyz"]))
